653fca8d419a09ca0a730ae90319427f616e2d1c
Greg Doud <gpdoud@gmail.com>
2020-02-21 16:06:56 -0500

    Add project files.

---
+++ LinqAndExtensionMethods/ExtenstionMethods.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqAndExtensionMethods {
+    static class ExtenstionMethods {
+
+        public static string Hello(this int i) {
+            return $"Hello number {i}";
+        }
+    }
+}
